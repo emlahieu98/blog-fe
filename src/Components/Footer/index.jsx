a4424e3dd9b8e72f479e1bf5639281b8d3fc74c4
emlahieu98 <hieucr98tb@gmail.com>
2022-11-17 17:52:13 +0700

    feat: footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="py-8 lg:pt-16 lg:pb-10 bg-[#3d3c3c] px-4">
-            <div className="container mx-auto ">
-                <section className="flex  mt-5">
-                    <h3 className=" mb-2 text-sm">
-                        © 2022 All rights reserved.
-                    </h3>
+        <footer className="py-8 lg:pt-16 lg:pb-10 bg-[#ececec] px-4 ">
+            <div className="container mx-auto">
+                <section className="flex justify-center">
+                    <i className="text-sm text-xl">
+                        " Your love makes me strong, your hate makes me
+                        unstoppable "
+                    </i>
                 </section>
             </div>
         </footer>
